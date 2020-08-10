d7f033672627a20e13c5d40ae36958eab0f5a83a
vorsurm <moshiur1412@gmail.com>
2020-08-11 03:00:39 +0600

    Updated some content header

--- resources/views/crud/create.blade.php
+++ resources/views/crud/create.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Create New</h2>
+            <h2>Create Page</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('crud.index') }}"> Back</a>
